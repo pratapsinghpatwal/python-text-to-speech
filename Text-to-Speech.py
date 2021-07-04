3e8d2f7934463a6c3edb017e35101b44f2fdb639
pratapsinghpatwal <60190354+pratapsinghpatwal@users.noreply.github.com>
2021-07-04 17:32:13 +0530

    Add files via upload

---
+++ Text-to-Speech.py
@@ -0,0 +1,61 @@
+## import libraries
+
+from tkinter import *
+from gtts import gTTS
+from playsound import playsound
+import os
+
+
+
+################### Initialized window####################
+
+root = Tk()
+root.geometry('350x300')
+root.resizable(0,0)
+root.config(bg = 'ghost white')
+root.title('TEXT_TO_SPEECH')
+
+
+##heading
+Label(root, text = 'TEXT_TO_SPEECH' , font='arial 20 bold' , bg ='white smoke').pack()
+Label(root, text ='Pratap Singh Patwal' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
+
+
+
+
+#label
+Label(root, text ='Enter Text', font ='arial 15 bold', bg ='white smoke').place(x=20,y=60)
+
+
+##text variable
+Msg = StringVar()
+
+
+#Entry
+entry_field = Entry(root,textvariable =Msg, width ='50')
+entry_field.place(x=20 , y=100)
+
+
+###################define function##############################
+
+def Text_to_speech():
+    
+    Message = entry_field.get()
+    speech = gTTS(text = Message)
+    speech.save('bhanu.mp3')
+    playsound('bhanu.mp3')
+    os.remove('bhanu.mp3')
+def Exit():
+    root.destroy()
+
+def Reset():
+    Msg.set("")
+
+#Button
+Button(root, text = "PLAY" , font = 'arial 15 bold', command = Text_to_speech, width =4).place(x=25, y=140)
+Button(root,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed').place(x=100,y=140)
+Button(root, text = 'RESET', font='arial 15 bold', command = Reset).place(x=175 , y =140)
+
+
+#infinite loop to run program
+root.mainloop()
